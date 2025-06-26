5e8363e9f3a9d2304dba14520f7d4acf310cee0f
齊藤拓海 <takumi1222122212221222@gmail.com>
2025-06-27 08:14:52 +0900

    確認部分の削除だけ

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,9 +22,6 @@ class ArticlesController < ApplicationController
 
   private
   def article_params
-    puts "----------------------"
-    puts params
-    puts "-----------------------"
     params.require(:article).permit(:title, :content)
   end
 end
