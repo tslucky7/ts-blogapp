071742615ad0191b4475bb90e574ca026c1dcc14
齊藤拓海 <takumi1222122212221222@gmail.com>
2025-09-11 00:07:35 +0900

    user_idのリレーション、マイグレーションファイルの作成、マイグレーションの実行・データベースの更新

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2025_09_03_153017) do
+ActiveRecord::Schema[7.2].define(version: 2025_09_10_143751) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_articles_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
