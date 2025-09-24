376c7102bb50721e8b1e2379a35855b72627fed9
齊藤拓海 <takumi1222122212221222@gmail.com>
2025-09-24 23:51:02 +0900

    年齢の計算・表示

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -19,4 +19,21 @@
 class Profile < ApplicationRecord
   enum gender: { male: 0, female: 1, other: 2 }
   belongs_to :user
+
+  def age
+    # 誕生日が未入力（nilまたは空）の場合は年齢を計算できないため、「不明」という文字列を返す処理です。
+    return "不明" unless birthday.present?
+    # 現在の年から誕生年を引いて年齢を計算
+    years = Time.zone.now.year - birthday.year
+    # 現在の日付と誕生日の日付を比較して、誕生日が過ぎているかどうかを判断
+    days = Time.zone.now.yday - birthday.yday
+
+    if days < 0
+      # 誕生日が過ぎていない場合は年齢を1つ減らして表示
+      "#{years - 1}歳"
+    else
+      # 誕生日が過ぎている場合は年齢をそのまま表示
+      "#{years}歳"
+    end
+  end
 end
