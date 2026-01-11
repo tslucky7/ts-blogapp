77a4b25926cb5dc828e1ac5452a360639616e1dc
齊藤拓海 <takumi1222122212221222@gmail.com>
2026-01-11 20:50:20 +0900

    feat: フォロー機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -28,6 +28,12 @@ class User < ApplicationRecord
   # through: :likes likesテーブルを介してarticleテーブルにアクセスする
   # source: :article articleテーブルを参照するという意味。すでに:articleが指定されているのに加え、お気に入り記事というのがわかりづらいため
   has_many :favorite_articles, through: :likes, source: :article
+  has_many :following_relationships, foreign_key: "follower_id", class_name: "Relationship", dependent: :destroy
+  has_many :followings, through: :following_relationships, source: :following
+
+  has_many :follower_relationships, foreign_key: "following_id", class_name: "Relationship", dependent: :destroy
+  has_many :followers, through: :follower_relationships, source: :follower
+
   has_one :profile, dependent: :destroy
 
   # delegateメソッドを使用することによりメソッドの記載を簡略化できる
@@ -57,6 +63,15 @@ class User < ApplicationRecord
     # ボッチ演算子では上記のコードを以下のように書き換えることができる
     profile&.nickname || self.email.split("@")[0]
     # -> ["cohki0305", "gmail.com"]
+  end
+
+  def follow!(user)
+    following_relationships.create!(following_id: user.id)
+  end
+
+  def unfollow!(user)
+    relation = following_relationships.find_by!(following_id: user.id)
+    relation.destroy!
   end
 
   def prepare_profile
